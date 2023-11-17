49c546a1a59dd548d2715abaadaa2a22bdf18bcc
Fredy Alejandro Russi Fajardo <frussi@asesoftware.com>
2023-11-16 21:45:05 -0500

    Persistencia finalizado

--- src/aerolinea/aerolinea.entity.ts
+++ src/aerolinea/aerolinea.entity.ts
@@ -1,1 +1,44 @@
-export class AerolineaEntity {}
+import { IsNumber, IsNotEmpty, IsString, MaxLength, MinLength, IsDate, MaxDate, IsUrl } from '@nestjs/class-validator';
+import { Transform } from 'class-transformer';
+import { AeropuertoEntity } from 'src/aeropuerto/aeropuerto.entity';
+import { PrimaryGeneratedColumn, Column, ManyToMany, Entity } from 'typeorm';
+
+@Entity()
+export class AerolineaEntity {
+    @PrimaryGeneratedColumn('uuid')
+    @IsNumber()
+    @IsNotEmpty()
+    id: number;
+
+    @Column()
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(20)
+    @MinLength(3)
+    nombre: string;
+
+    @Column()
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(100)
+    @MinLength(3)
+    descripcion: string;
+
+    @Column()
+    @IsNotEmpty()
+    @Transform(({ value }) => new Date(value))
+    @IsDate()
+    @MaxDate(new Date())
+    fechaFundacion: Date;
+
+    @Column()
+    @IsNotEmpty()
+    @IsUrl()
+    urlPaginaWeb: string;
+
+    @ManyToMany(() => AeropuertoEntity, (aeropuerto) => aeropuerto.aerolineas, {
+        onDelete: 'NO ACTION',
+        onUpdate: 'NO ACTION',
+    })
+    aeropuertos: AeropuertoEntity[];
+}
