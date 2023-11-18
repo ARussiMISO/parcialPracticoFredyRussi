ac44aae4fafcf87bb1fab2bce3944b9d2c9f6563
Fredy Alejandro Russi Fajardo <frussi@asesoftware.com>
2023-11-18 16:39:19 -0500

    Fin de pruebas de aerolinea-aeropuerto.service

--- src/aerolinea-aeropuerto/aerolinea-aeropuerto.service.spec.ts
+++ src/aerolinea-aeropuerto/aerolinea-aeropuerto.service.spec.ts
@@ -67,17 +67,17 @@ describe('AerolineaAeropuertoService', () => {
 
     const newAirport: AeropuertoEntity = await repositoryAirport.save({
       nombre: faker.company.name(),
-      ciudad : faker.lorem.sentence(),
-      pais : faker.lorem.sentence(),
-      codigo : faker.string.alphanumeric({length: 3}),
+      ciudad: faker.lorem.sentence(),
+      pais: faker.lorem.sentence(),
+      codigo: faker.string.alphanumeric({ length: 3 }),
       aerolineas: []
     });
 
     const newAirline: AerolineaEntity = await repositoryAirline.save({
       nombre: faker.company.name(),
       descripcion: faker.lorem.text(),
-      fechaFundacion : faker.date.past({ refDate:'2023-11-15T00:00:00.000Z'}),
-      urlPaginaWeb : faker.internet.url(),
+      fechaFundacion: faker.date.past({ refDate: '2023-11-15T00:00:00.000Z' }),
+      urlPaginaWeb: faker.internet.url(),
       aeropuertos: []
     });
 
@@ -96,8 +96,8 @@ describe('AerolineaAeropuertoService', () => {
     const newAirline: AerolineaEntity = await repositoryAirline.save({
       nombre: faker.company.name(),
       descripcion: faker.lorem.text(),
-      fechaFundacion : faker.date.past({ refDate:'2023-11-15T00:00:00.000Z'}),
-      urlPaginaWeb : faker.internet.url(),
+      fechaFundacion: faker.date.past({ refDate: '2023-11-15T00:00:00.000Z' }),
+      urlPaginaWeb: faker.internet.url(),
       aeropuertos: []
     });
 
@@ -108,9 +108,9 @@ describe('AerolineaAeropuertoService', () => {
 
     const newAirport: AeropuertoEntity = await repositoryAirport.save({
       nombre: faker.company.name(),
-      ciudad : faker.lorem.sentence(),
-      pais : faker.lorem.sentence(),
-      codigo : faker.string.alphanumeric({length: 3}),
+      ciudad: faker.lorem.sentence(),
+      pais: faker.lorem.sentence(),
+      codigo: faker.string.alphanumeric({ length: 3 }),
       aerolineas: []
     });
 
@@ -135,6 +135,100 @@ describe('AerolineaAeropuertoService', () => {
 
   it('findAirportFromAirline should throw an exception for an invalid airline', async () => {
     await expect(() => service.findAirportFromAirline(0, airportList[0].id)).rejects.toHaveProperty("message", "La aerolinea con el id dado no encontrada");
+  });
+
+  it('findAirportFromAirline should throw an exception for an airport not associated to the airline', async () => {
+    const newAirport: AeropuertoEntity = await repositoryAirport.save({
+      nombre: faker.company.name(),
+      ciudad: faker.lorem.sentence(),
+      pais: faker.lorem.sentence(),
+      codigo: faker.string.alphanumeric({ length: 3 }),
+      aerolineas: []
+    });
+
+    await expect(() => service.findAirportFromAirline(airline.id, newAirport.id)).rejects.toHaveProperty("message", "El aeropuerto con el id dado no está asociado a la aerolínea");
+  });
+
+  it('findAirportsFromAirline should return airports by airline', async () => {
+    const airports: AeropuertoEntity[] = await service.findAirportsFromAirline(airline.id);
+    expect(airports.length).toBe(5);
+  });
+
+  it('findAirportsFromAirline should throw an exception for an invalid airline', async () => {
+    await expect(() => service.findAirportsFromAirline(0)).rejects.toHaveProperty("message", "La aerolinea con el id dado no encontrada");
+  });
+
+  it('updateAirportsFromAirline should update airports list for a airline', async () => {
+    const newAirport: AeropuertoEntity = await repositoryAirport.save({
+      nombre: faker.company.name(),
+      ciudad: faker.lorem.sentence(),
+      pais: faker.lorem.sentence(),
+      codigo: faker.string.alphanumeric({ length: 3 }),
+      aerolineas: []
+    });
+
+    const updatedAirline: AerolineaEntity = await service.updateAirportsFromAirline(airline.id, [newAirport]);
+
+    expect(updatedAirline.aeropuertos.length).toBe(1);
+    expect(updatedAirline.aeropuertos[0]).not.toBeNull();
+    expect(updatedAirline.aeropuertos[0].nombre).toEqual(newAirport.nombre);
+    expect(updatedAirline.aeropuertos[0].ciudad).toEqual(newAirport.ciudad);
+    expect(updatedAirline.aeropuertos[0].codigo).toEqual(newAirport.codigo);
+    expect(updatedAirline.aeropuertos[0].pais).toEqual(newAirport.pais);
+  });
+
+  it('updateAirportsFromAirline should throw an exception for an invalid airline', async () => {
+
+    const newAirport: AeropuertoEntity = await repositoryAirport.save({
+      nombre: faker.company.name(),
+      ciudad: faker.lorem.sentence(),
+      pais: faker.lorem.sentence(),
+      codigo: faker.string.alphanumeric({ length: 3 }),
+      aerolineas: []
+    });
+
+    await expect(() => service.updateAirportsFromAirline(0, [newAirport])).rejects.toHaveProperty("message", "La aerolinea con el id dado no encontrada");
+
+  });
+
+  it('updateAirportsFromAirline should throw an exception for an invalid airport', async () => {
+
+    const newAirport: AeropuertoEntity = airportList[0];
+    newAirport.id = 0;
+
+    await expect(() => service.updateAirportsFromAirline(airline.id, [newAirport])).rejects.toHaveProperty("message", "El aeropuerto con el id dado no encontrado");
+
+  });
+
+  it('deleteAirportFromAirline should remove an airport from a airline', async () => {
+    const airport: AeropuertoEntity = airportList[0];
+
+    await service.deleteAirportFromAirline(airline.id, airport.id);
+
+    const storedAirline: AerolineaEntity = await repositoryAirline.findOne({where: {id: airline.id}, relations: ["aeropuertos"]});
+    const deletedAirport: AeropuertoEntity = storedAirline.aeropuertos.find(a => a.id === airport.id);
+    expect(deletedAirport).toBeUndefined();
+  });
+
+  it('deleteAirportFromAirline should thrown an exception for an invalid airport', async () => {
+    await expect(()=> service.deleteAirportFromAirline(airline.id, 0)).rejects.toHaveProperty("message", "El aeropuerto con el id dado no encontrado");
+  });
+
+  it('deleteAirportFromAirline should thrown an exception for an invalid airline', async () => {
+    const airport: AeropuertoEntity = airportList[0];
+    await expect(()=> service.deleteAirportFromAirline(0, airport.id)).rejects.toHaveProperty("message", "La aerolinea con el id dado no encontrada");
+  });
+  it('deleteAirportFromAirline should thrown an exception for an non asocciated airport', async () => {
+    const newAirport: AeropuertoEntity = await repositoryAirport.save({
+      nombre: faker.company.name(),
+      ciudad: faker.lorem.sentence(),
+      pais: faker.lorem.sentence(),
+      codigo: faker.string.alphanumeric({ length: 3 }),
+      aerolineas: []
+    });
+
+    await expect(()=> service.deleteAirportFromAirline(airline.id, newAirport.id)).rejects.toHaveProperty("message", "El aeropuerto con el id dado no está asociado a la aerolínea");
+
   });
 
 });
